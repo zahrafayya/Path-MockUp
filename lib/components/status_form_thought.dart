a72e240161d5dadaa19c8e9944be502faa46bfdd
Zahra Fayyadiyati <zahra.fayyadiyati@gmail.com>
2024-06-04 13:42:21 +0700

    feat: layout for music, location and thought

---
+++ lib/components/status_form_thought.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class StatusFormThought extends StatelessWidget {
+  final String? thought;
+  final ValueChanged<String> onChangedThought;
+
+  const StatusFormThought({
+    Key? key,
+    this.thought = '',
+
+    required this.onChangedThought,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start, // Ensure children are aligned to the start
+          children: [
+            Text(
+              'I\'m currently think about -',
+              style: TextStyle(
+                height: 2.5,
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            buildTitle(),
+          ],
+        ),
+      )
+  );
+
+  Widget buildTitle() => TextFormField(
+    maxLines: 1,
+    initialValue: thought,
+    decoration: const InputDecoration(
+      border: OutlineInputBorder(),
+      hintText: 'Write your thought here.',
+      hintStyle: TextStyle(
+        color: Colors.grey,
+        fontWeight: FontWeight.normal
+    ),
+    ),
+    validator: (locationName) => locationName != null && locationName.isEmpty ? 'The location cannot be empty' : null,
+    onChanged: onChangedThought,
+  );
+}
+
--- lib/pages/app/home_page.dart
+++ lib/pages/app/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
+import 'package:path_mock_up/pages/app/create_status_page.dart';
 import '../layout/bottom_navbar.dart';
 
 class HomePage extends StatefulWidget {
@@ -18,11 +19,41 @@ class _HomePageState extends State<HomePage> {
     FirebaseAuth.instance.signOut();
   }
 
+  void createStatusChangePage(String statusType) {
+    Navigator.push(
+      context,
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) => CreateStatusPage(statusType: statusType),
+          transitionDuration: Duration(milliseconds: 300),
+          reverseTransitionDuration: Duration(milliseconds: 300),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            const begin = Offset(0.0, 1.0);
+            const end = Offset(0.0, 0.0);
+            const curve = Curves.ease;
+
+            var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+            var offsetAnimation = animation.drive(tween);
+
+            return SlideTransition(
+              position: offsetAnimation,
+              child: child,
+            );
+          },
+        )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        centerTitle: true,
+        title: Image.asset('lib/images/path_text.png', height: 24),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu, color: Colors.white, size: 30),
+        ),
+        backgroundColor: Color(0xFFC03027),
       ),
       body: Center(
         child: Text("Home Content"),
@@ -59,21 +90,21 @@ class _HomePageState extends State<HomePage> {
             backgroundColor: Colors.black,
             shape: const CircleBorder(),
             child: const Icon(Icons.format_quote_sharp, color: Colors.white,),
-            onPressed: () {},
+            onPressed: () => createStatusChangePage('Thought'),
           ),
           FloatingActionButton.small(
             heroTag: null,
             backgroundColor: Colors.black,
             shape: const CircleBorder(),
             child: const Icon(Icons.music_note_sharp, color: Colors.white,),
-            onPressed: () {},
+            onPressed: () => createStatusChangePage('Music'),
           ),
           FloatingActionButton.small(
             heroTag: null,
             backgroundColor: Colors.black,
             shape: const CircleBorder(),
             child: const Icon(Icons.location_pin, color: Colors.white,),
-            onPressed: () {},
+            onPressed: () => createStatusChangePage('Location'),
           ),
           FloatingActionButton.small(
             heroTag: null,
